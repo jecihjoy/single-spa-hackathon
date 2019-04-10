b799a6f0d71256fe1cd9901ad20638a29702a7a8
Emmanuel Nyachoke <achachiez@gmail.com>
2019-04-10 16:55:05 +0300

    Remove hardcorded uuid

--- patientdashboard/src/app/services/vitals.service.ts
+++ patientdashboard/src/app/services/vitals.service.ts
@@ -8,7 +8,7 @@ export class VitalsService {
     constructor(private httpClient: HttpClient) { }
     fetchVitals(patientUuid) {
         let url =  Constants.baseETLURL;
-        url += '/patient/' + 'ce1b2ab4-6cb6-4483-a96d-d4b26ef54f0e/vitals';
+        url += '/patient/' + patientUuid + '/vitals';
 
         const params: HttpParams = new HttpParams()
             .set('startIndex', ('0'))
