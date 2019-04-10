7948522b001632b302e51479201a67cdb3b6ae9a
Emmanuel Nyachoke <achachiez@gmail.com>
2019-04-10 16:19:15 +0300

    Add vitals,encounters,patient info

--- patientdashboard/src/app/patient-info/patient-info.component.ts
+++ patientdashboard/src/app/patient-info/patient-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PatientService } from '../services/patient.service';
 
 @Component({
   selector: 'app-patient-info',
@@ -7,13 +8,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./patient-info.component.css']
 })
 export class PatientInfoComponent implements OnInit {
+  patient: any;
+  constructor(private activeRoute: ActivatedRoute, private patientService: PatientService) {
+  }
   ngOnInit(): void {
-    this.activeRoute.params.subscribe(params => {
+    this.activeRoute.parent.params.subscribe(params => {
       if (params.patient_uuid) {
-        console.log('Fetch Patient info for patient', params.patient_uuid);
+        console.log('Fetch Patient with', params.patient_uuid);
+        this.patientService.fetchpatient(params.patient_uuid).subscribe((patient) => {
+          this.patient = patient;
+        });
       }
     });
-  }
-  constructor(private activeRoute: ActivatedRoute) {
+
   }
 }
---
+++ patientdashboard/src/app/services/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+    public static get baseOpenmrsURL(): string { return 'https://ngx.ampath.or.ke/amrs/ws/rest/v1'; }
+    public static get baseETLURL(): string { return 'https://ngx.ampath.or.ke/etl-latest/etl'; }
+}
