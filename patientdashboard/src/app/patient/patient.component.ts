61404f2f127e488c3469c5424c8fb5a5891ee33a
Emmanuel Nyachoke <achachiez@gmail.com>
2019-04-10 14:50:16 +0300

    Add auth

--- patientdashboard/src/app/patient/patient.component.ts
+++ patientdashboard/src/app/patient/patient.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PatientService } from '../services/patient.service';
 
 @Component({
   selector: 'app-patient',
@@ -7,10 +8,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./patient.component.css']
 })
 export class PatientComponent {
-  constructor(private activeRoute: ActivatedRoute) {
+  patient: any;
+  constructor(private activeRoute: ActivatedRoute, private patientService: PatientService) {
     this.activeRoute.params.subscribe(params => {
       if (params.patient_uuid) {
         console.log('Fetch Patient with', params.patient_uuid);
+        this.patientService.fetchpatient('ce1b2ab4-6cb6-4483-a96d-d4b26ef54f0e').subscribe((patient) => {
+          this.patient = patient;
+        });
       }
     });
 
---
+++ patientdashboard/src/app/services/poc-http-interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AutheticationService } from './authentication.service';
+import { Router } from '@angular/router';
+@Injectable()
+export class PocHttpInteceptor implements HttpInterceptor {
+  constructor(private autheticationService: AutheticationService, private router: Router) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const credentials = this.autheticationService.getAuth();
+    let modifiedReq = req;
+    if (credentials) {
+      const authHeader = { Authorization: 'Basic ' + credentials };
+      modifiedReq = req.clone({ setHeaders: authHeader });
+    } else {
+      this.router.navigate(['/login']);
+    }
+    return next.handle(modifiedReq).pipe(tap(event => {
+      if (event instanceof HttpResponse) {
+        // do stuff with response here
+      }
+    }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        return observableThrowError(err);
+      }
+    }));
+  }
+}
