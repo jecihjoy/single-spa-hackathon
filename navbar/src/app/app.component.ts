a9c1bbda3d255911d915a8e72e0453af6a97712b
jecihjoy <jecihjoy@gmail.com>
2019-04-10 17:41:23 +0300

    redirect if credentials is not set

--- navbar/src/app/app.component.ts
+++ navbar/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'navbar-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'navbar';
+  constructor(private router: Router){
+
+  }
+  ngOnInit(){
+
+    if(this.getAuth = null){
+      console.log("called");
+      this.router.navigate(['/login']);
+    }
+  }
+
+  getAuth() {
+    const auth = sessionStorage.getItem('auth.credentials');
+    return auth;
+  }
 }
